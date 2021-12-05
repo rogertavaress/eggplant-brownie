ff5f2c40bfed9e1a8e25603cbce31ed7ace56820
Rogério Tavares <rogerioctf@gmail.com>
2021-12-05 09:29:25 -0300

    aprendendo sobre TableView

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -11,12 +11,64 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    // MARK: - Atributos
     
     var delegate: AdicionaRefeicaoDelegate?
+//    var itens: [String] = [
+//        "Molho de tomate", "Queijo", "Molho apimentado", "Manjericão"
+//    ]
+    var itens: [Item] = [
+        Item(nome: "Molho de tomate", calorias: 40.0),
+        Item(nome: "Queijo", calorias: 40.0),
+        Item(nome: "Molho apimentado", calorias: 40.0),
+        Item(nome: "Manjericão", calorias: 40.0),
+    ]
+    
+    var itensSelecionados: [Item] = []
+    
+    // MARK: - IBOutlets
     
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet var felicidadeTextField: UITextField?
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let linhaDaTabela = indexPath.row
+        let item = itens[linhaDaTabela]
+        
+        celula.textLabel?.text = item.nome
+        
+        return celula
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        
+        if celula.accessoryType == .none {
+            celula.accessoryType = .checkmark
+            let linhaDaTabela = indexPath.row
+            itensSelecionados.append(itens[linhaDaTabela])
+        } else {
+            celula.accessoryType = .none
+            let item = itens[indexPath.row]
+            if let position = itensSelecionados.firstIndex(of: item) {
+                itensSelecionados.remove(at: position)
+            }
+            
+        }
+        
+    }
+    
+    // MARK: - IBActions
     
     @IBAction func adicionar(_ sender: Any) {
         guard let nomeDaRefeicao = nomeTextField?.text else {
@@ -27,7 +79,7 @@ class ViewController: UIViewController {
             return;
         }
         
-        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade, itens: itensSelecionados)
         
         delegate?.add(refeicao)
         navigationController?.popViewController(animated: true)
--- eggplant-brownie/Models/Refeicao.swift
+++ eggplant-brownie/Models/Refeicao.swift
@@ -10,11 +10,12 @@ import UIKit
 class Refeicao: NSObject {
     let nome: String
     let felicidade: Int
-    let itens: Array<Item> = []
+    var itens: Array<Item> = []
     
-    init(nome: String, felicidade: Int) {
+    init(nome: String, felicidade: Int, itens: [Item] = []) {
         self.nome = nome
         self.felicidade = felicidade
+        self.itens = itens
     }
     
     func totalDeCalorias() -> Double {
